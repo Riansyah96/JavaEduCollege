a6e62ca091b60e6b02429454a2ed22e1a3379dbf
Riansyah96 <arkalbojong@gmail.com>
2025-10-30 14:02:45 +0700

    Praktikum Abstrak Pa Nasrul

---
+++ src/main/java/Abstractt/Anjing.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Abstractt;
+
+/**
+ *
+ * @author apriansyah
+ */
+public class Anjing extends Hewan {
+
+    @Override
+    public void bersuara() {
+        System.out.println("Suara Anjing : Guk.... Guk...Guk....");
+    }
+}
---
+++ src/main/java/Abstractt/Kambing.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Abstractt;
+
+/**
+ *
+ * @author apriansyah
+ */
+public class Kambing extends Hewan {
+
+    @Override
+    public void bersuara() {
+        System.out.println("Suara Kucing : Mbeeee...Mbeeee....");
+    }
+}
